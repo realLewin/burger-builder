9a03cc4c9c3f74dd281894b18c6e7e515fd60601
integreator <walterfrontier@gmail.com>
2020-10-12 08:08:43 -0400

    show ingredient to the screen

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "../Burger/BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
+  let transformIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((acc, cur) => acc.concat(cur), []);
+
+  if (transformIngredients.length === 0) {
+    transformIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="bacon" />
+      {transformIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
